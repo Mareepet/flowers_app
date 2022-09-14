4410756753fe4148d7bfcc874aabcfff51191652
mareeswaripetchimuthu <venkyozzie790@gmail.com>
2022-09-14 10:00:26 +0930

    flowers_app version_1

--- db/db.rb
+++ db/db.rb
@@ -2,7 +2,8 @@
 require 'pg'
 
 def run_sql(sql,sql_params = [])
-    db = PG.connect(dbname: 'flowers_app')
+    # db = PG.connect(dbname: 'flowers_app')
+    db = PG.connect(ENV['DATABASE_URL'] || {dbname: 'flowers_app'})
     results = db.exec_params(sql,sql_params)
     db.close
     results
